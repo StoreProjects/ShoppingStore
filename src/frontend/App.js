a2db5457bf0de76d1fcedd3e895dfcaf6a287ef3
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-08-21 19:40:05 -0500

    Set up frontend prototype

---
+++ src/frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      Shopping Star Prototype
+    </div>
+  );
+}
+
+export default App;
